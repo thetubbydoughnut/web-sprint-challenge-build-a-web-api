9cc218ee26eb181c33c3ff21773c85153c28e4d2
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-09-21 13:39:32 -0500

    Got the Projects Router done

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,7 @@
 // Write your "projects" router here!
 const express = require('express');
 const Projects = require('./projects-model')
+const Actions = require('../actions/actions-model')
 const { validateProjectId, validateProject,} = require('./projects-middleware')
 
 const router = express.Router();
@@ -26,7 +27,31 @@ router.post('/', validateProject, (req, res, next) => {
     .catch(next)
 })
 
+router.put('/:id', validateProjectId, validateProject, (req, res, next) => {
+    const { id } = req.params
 
+    Projects.update(id, req.body)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(next)
+})
+
+router.delete('/:id', validateProjectId, (req, res, next) => {
+    Projects.remove(req.params.id)
+    .then(
+        res.status(200).send()
+    )
+    .catch(next)
+})
+
+router.get('/:id/actions', validateProjectId, (req, res, next) => {
+    Actions.get(req.params.id)
+    .then(actions =>
+       res.status(200).json(actions) 
+    )
+    .catch(next)
+})
 
 router.use((error, req, res, next) => { //eslint-disable-line
     res.status(error.status || 500).json({
